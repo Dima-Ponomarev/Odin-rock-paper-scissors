f76e6cced3710c919ab8e7ae97e0b16ce9383af0
Dima Ponomarev <kikkyow18@gmail.com>
2021-01-14 12:14:37 +0400

    Add computerPlay function

---
+++ app.js
@@ -0,0 +1,18 @@
+const computerPlay = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber){
+        case 0: return "Rock"; 
+        case 1: return "Paper"; 
+        case 2: return "Scissors"; 
+    }
+}
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+
