e304c4a46d43671c38352f122c1334fe30c812de
Dima Ponomarev <kikkyow18@gmail.com>
2021-01-14 12:46:58 +0400

    Add playRound function

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ const computerPlay = () => {
         case 2: return "Scissors"; 
     }
 }
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+
+const playRound = (playerSelection, computerSelection) => {
+    const formattedPS = playerSelection.toLowerCase();
+    const formattedCS = computerSelection.toLowerCase();
+
+    if (formattedPS === formattedCS){
+        return "It's a tie!";
+    } else if (formattedPS === "rock" && formattedCS === "paper"){
+        return "You Lose! Paper beats Rock";
+    } else if (formattedPS === "rock" && formattedCS === "scissors"){
+        return "You Win! Rock beats Scissors";
+    } else if (formattedPS === "paper" && formattedCS === "rock"){
+        return "You Win! Paper beats Rock";
+    } else if (formattedPS === "paper" && formattedCS === "scissors"){
+        return "You Lose! Scissors beats Paper";
+    } else if (formattedPS === "scissors" && formattedCS === "paper"){
+        return "You Win! Scissors beats Paper";
+    } else if (formattedPS === "scissors" && formattedCS === "rock"){
+        return "You Lose! Rock beats Scissors";
+    } 
+
+}
 
